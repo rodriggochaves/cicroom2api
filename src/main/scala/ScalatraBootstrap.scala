e278d83e9a421116ce020ae9440cecaf0977ca94
rodriggochaves <rodriggo.chaves@gmail.com>
2017-11-10 14:48:47 -0200

    ScalaBootstrap configura conexao no banco

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,30 @@
-import com.github.rodriggochaves.api._
+import com.github.rodriggochaves.cicroomapi._
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import org.slf4j.LoggerFactory
 import org.scalatra._
 import javax.servlet.ServletContext
+import slick.jdbc.JdbcBackend.Database
 
 class ScalatraBootstrap extends LifeCycle {
+  
+  val logger = LoggerFactory.getLogger(getClass)
+
+  val cpds = new ComboPooledDataSource
+  logger.info("Created c3p0 connection pool")
+
   override def init(context: ServletContext) {
-    context.mount(new RoomsController, "/api/*")
+    val db = Database.forDataSource(cpds, None)
+    context.mount(new UsersController(db), "/api/users/")
+    context.mount(new RoomsController(db), "/api/rooms/")
+  }
+
+  private def closeDbConnection() {
+    logger.info("Closing c3po connection pool")
+    cpds.close
+  }
+
+  override def destroy(context: ServletContext) {
+    super.destroy(context)
+    closeDbConnection
   }
 }
