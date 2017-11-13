f48907bc5f8952bee34c72379e3163fb85192447
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-13 14:32:37 -0200

    isolando controller de Rooms

---
+++ src/main/scala/com/cicroomapi/controllers/RoomsController.scala
@@ -0,0 +1,17 @@
+package com.cicroomapi.controllers
+
+import org.scalatra._
+import org.scalatra.json._
+import org.json4s.JsonAST._
+import org.json4s.{DefaultFormats, Formats}
+import slick.driver.PostgresDriver.api._
+
+class RoomsController(val db: Database) extends ScalatraServlet  with JacksonJsonSupport {
+
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats
+  
+  post("/") {
+    val hello = "Hello"
+    hello
+  }
+}
