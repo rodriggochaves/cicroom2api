384919adf18a2197d5c6df27e42eede65eec31a0
diogoqds <diogoqueirozsantos97@gmail.com>
2017-11-16 11:37:18 -0200

    Correção da description

--- src/main/scala/com/cicroomapi/models/RoleModel.scala
+++ src/main/scala/com/cicroomapi/models/RoleModel.scala
@@ -11,7 +11,7 @@ object RoleModel {
   def create = {
     db.run( roles.map( c => (c.description) ) += "Aluno" )
     db.run( roles.map( c => (c.description) ) += "Coordenador" )
-    db.run( roles.map( c => (c.description) ) += "Adminstrador" )
+    db.run( roles.map( c => (c.description) ) += "Administrador" )
   }
 
 
