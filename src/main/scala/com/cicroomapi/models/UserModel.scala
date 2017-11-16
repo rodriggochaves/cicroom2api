4339f84fd995da077b741807de04464005b50366
diogoqds <diogoqueirozsantos97@gmail.com>
2017-11-16 11:39:09 -0200

    Temos uma fk, então alterei o nome da coluna

--- src/main/scala/com/cicroomapi/models/UserModel.scala
+++ src/main/scala/com/cicroomapi/models/UserModel.scala
@@ -1,6 +1,8 @@
 package com.cicroomapi.models
 
 import slick.driver.PostgresDriver.api._
+import slick.dbio.DBIOAction._
+
 
 object UserModel {
   
@@ -9,7 +11,11 @@ object UserModel {
   val users = TableQuery[UsersTable]
 
   def create( list: (String, String, String,Int)  ) = {
-    db.run( users.map( c => (c.username, c.email, c.digest_password, c.role) ) += list )
+    db.run( users.map( c => (c.username, c.email, c.digest_password, c.roleId) ) += list )
   }
+
+  // def select(list: (String) ) = {
+  // 	db.run( users.filter(_.email === list))
+  // }
 
 }
