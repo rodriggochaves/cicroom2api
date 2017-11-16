75e1ca9e47ea2ac9f5b279b52f93f23755e795af
diogoqds <diogoqueirozsantos97@gmail.com>
2017-11-16 10:40:47 -0200

    Alterado para aceitar a tabela roles

--- src/main/scala/com/cicroomapi/models/TableSchema.scala
+++ src/main/scala/com/cicroomapi/models/TableSchema.scala
@@ -7,12 +7,15 @@ object TableSchema {
   
   val rooms = TableQuery[RoomsTable]
   val users = TableQuery[UsersTable]
+  val roles = TableQuery[RolesTable]
 
-  val schema = rooms.schema ++ users.schema
+  val schema = rooms.schema ++ users.schema ++ roles.schema
 
   val dropSchemaAction = schema.drop
   val createSchemaAction = schema.create
 
   // def createUser = users.map( c => (c.username, c.email, c.digest_password) ) 
   // += ("rbonifacio", "rodrigo123@email.com", "password")
+
+  def createRoles = RoleModel.create
 }
