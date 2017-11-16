437019da7e137f8f67965065b0133cfdb742a25f
diogoqds <diogoqueirozsantos97@gmail.com>
2017-11-16 10:41:30 -0200

    Adicionada a coluna de role

--- src/main/scala/com/cicroomapi/models/UsersTable.scala
+++ src/main/scala/com/cicroomapi/models/UsersTable.scala
@@ -3,13 +3,14 @@ package com.cicroomapi.models
 import org.scalatra._
 import slick.driver.PostgresDriver.api._
 
-class UsersTable(tag: Tag) extends Table[(Int, String, String, String)](tag, "users") {
+class UsersTable(tag: Tag) extends Table[(Int, String, String, String,Int)](tag, "users") {
   
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def username = column[String]("username")
   def email = column[String]("email")
   def digest_password = column[String]("digest_password")
+  def role = column[Int]("role")
 
-  def * = (id, username, email, digest_password)
+  def * = (id, username, email, digest_password, role)
 
 }
