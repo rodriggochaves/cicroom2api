7a7a2e56065badc7ca4c14ee8251a4da5eac93f1
diogoqds <diogoqueirozsantos97@gmail.com>
2017-11-21 14:35:28 -0200

    Agora aceita numeros na senha

--- src/main/scala/com/cicroomapi/models/Cypher.scala
+++ src/main/scala/com/cicroomapi/models/Cypher.scala
@@ -1,12 +1,14 @@
 package com.cicroomapi.models
 
 object Cypher {
-  private val alphabet = ('A' to 'Z').toList
+  private val alphabet = ('A' to 'Z').toList ::: ('0' to '9').toList
 
   def encipher(key: String, text: String): String = {
     // Build the cipher alphabet by concatenating the key to
     // the alphabet and removing duplicate letters
     val cipherAlphabet = (key.toUpperCase.toList ::: alphabet).distinct
+
+    println(alphabet)
 
     val cipherText = for (c <- text.toUpperCase) yield c match {
       case ' ' => ' '
